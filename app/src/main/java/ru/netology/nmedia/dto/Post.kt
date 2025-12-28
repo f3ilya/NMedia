14eb34ce00e051180f740ab4c9b3704375fb6734
Ilya Paranin <ilya-paranin@mail.ru>
2025-12-28 23:07:07 +0300

    ex4

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -14,18 +14,18 @@ data class Post(
 fun counter(count: Long): String {
     val letter = when {
         count < 1_000_000 -> "K"
-        count in 1_000_000 .. 999_999_999 -> "M"
+        count in 1_000_000..999_999_999 -> "M"
         else -> "B"
     }
     val amount = when {
         count < 1_000_000 -> count
-        count in 1_000_000 .. 999_999_999 -> count / 1_000
+        count in 1_000_000..999_999_999 -> count / 1_000
         else -> count / 1_000_000
     }
 
     return when {
         count < 1_000 -> count.toString()
-        count in 10_000 .. 999_999 -> (count / 1_000).toString().plus("K")
+        count in 10_000..999_999 -> (count / 1_000).toString().plus(letter)
         else -> if (amount / 100 % 10 == 0L) {
             (amount / 1_000).toString().plus(letter)
         } else {
