16104fe60dde7802dae146befc06f1085e3927a3
Ilya Paranin <ilya-paranin@mail.ru>
2025-12-28 23:00:28 +0300

    ex4

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -5,16 +5,61 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.dto.counter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val padding = resources.getDimensionPixelSize(R.dimen.common_spacing)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left + padding,
+                systemBars.top + padding,
+                systemBars.right + padding,
+                systemBars.bottom + padding
+            )
             insets
+        }
+
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            likes = 1_999_999_999,
+            likedByMe = false,
+            share = 980,
+            views = 1_000_000
+        )
+
+        with(binding) {
+            avatar.setImageResource(R.drawable.ic_launcher_netology_48)
+            author.text = post.author
+            content.text = post.content
+            published.text = post.published
+            likeCount.text = counter(post.likes)
+            shareCount.text = counter(post.share)
+            viewsCount.text = counter(post.views)
+
+            like.setOnClickListener {
+                post.likedByMe = !post.likedByMe
+                like.setImageResource(
+                    if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
+                )
+                if (post.likedByMe) post.likes++ else post.likes--
+                likeCount.text = counter(post.likes)
+            }
+
+            share.setOnClickListener {
+                post.share++
+                shareCount.text = counter(post.share)
+            }
         }
     }
 }
