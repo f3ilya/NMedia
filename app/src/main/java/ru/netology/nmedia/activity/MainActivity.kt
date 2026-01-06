ad239c226037a89e8c5584ed1416e9a1d7ff84a6
Ilya Paranin <ilya-paranin@mail.ru>
2026-01-06 17:58:28 +0300

    ex6

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.counter
 import ru.netology.nmedia.viewmodule.PostViewModel
@@ -17,40 +18,20 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val padding = resources.getDimensionPixelSize(R.dimen.common_spacing)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(
-                systemBars.left + padding,
-                systemBars.top + padding,
-                systemBars.right + padding,
-                systemBars.bottom + padding
-            )
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         val viewModel: PostViewModel by viewModels()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                avatar.setImageResource(R.drawable.ic_launcher_netology_48)
-                author.text = post.author
-                content.text = post.content
-                published.text = post.published
-                likeCount.text = counter(post.likes)
-                shareCount.text = counter(post.share)
-                viewsCount.text = counter(post.views)
-                like.setImageResource(
-                    if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
-                )
-            }
-        }
-
-        binding.like.setOnClickListener {
-            viewModel.like()
-        }
-
-        binding.share.setOnClickListener {
-            viewModel.share()
+        val adapter = PostsAdapter (
+            { viewModel.likeById(it.id) },
+            { viewModel.shareById(it.id) }
+        )
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
         }
     }
 }
