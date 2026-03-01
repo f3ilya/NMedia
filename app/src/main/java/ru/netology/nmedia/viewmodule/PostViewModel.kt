6cbd2f100f029da4930083c7a1285a4e2276b5cb
Ilya Paranin <ilya-paranin@mail.ru>
2026-03-01 19:06:15 +0300

    ex11

--- app/src/main/java/ru/netology/nmedia/viewmodule/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodule/PostViewModel.kt
@@ -3,10 +3,10 @@ package ru.netology.nmedia.viewmodule
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.db.AppDb
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
-import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+import ru.netology.nmedia.repository.PostRepositorySQLiteImpl
 
 private val empty = Post(
     id = 0,
@@ -21,7 +21,9 @@ private val empty = Post(
 
 class PostViewModel(application: Application) : AndroidViewModel(application) {
     // упрощённый вариант
-    private val repository: PostRepository = PostRepositoryInMemoryImpl(application)
+    private val repository: PostRepository = PostRepositorySQLiteImpl(
+        AppDb.getInstance(application).postDao
+    )
     val data = repository.getAll()
     val edited = MutableLiveData(empty)
 
