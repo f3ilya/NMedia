6bd1d89792c063525f64ce00e5d5a38fd9139fd2
Ilya Paranin <ilya-paranin@mail.ru>
2026-08-11 21:02:22 +0300

    ex 3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,5 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.androidx.room.paging)
 }
